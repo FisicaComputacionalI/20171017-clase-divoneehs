9d3b551767b241f99f58cd70cfd0448950c65666
Diana Ivonee Huitzil Sosa <31254618+divoneehs@users.noreply.github.com>
2017-10-17 13:31:17 -0500

    Add files via upload
    Adjunto la suma de los impares, dio 25

---
+++ diagrama.cc
@@ -0,0 +1,24 @@
+//Programa que calcula la suma de numeros
+//impares del 1 al 10
+//Diana Ivonee Huitzil Sosa
+//17 de octubre, 2016
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int sum=0;
+  int count=1;
+
+  while (count<10) {
+    if ((count%2)>0)
+      {
+	sum=sum+count;
+      }
+    count++;
+  }
+ 
+  cout<<"suma"<<sum<<endl;
+ return 0;
+}
